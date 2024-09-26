d060e43c282f4c1521f6ac0ecc3851e156fc852b
mgarmer2811 <mgarmer2811@g.educaand.es>
2024-09-26 12:47:39 +0200

    Commit de Refactoring
    Refactoring de paquetes y clases en ambos proyectos

--- ACTIVIDAD_1_PRESENTACION/src/com/mgm/main/Main.java
+++ ACTIVIDAD_1_PRESENTACION/src/com/mgm/main/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package com.mgm;
+package com.mgm.main;
 
 /**
  *
@@ -13,15 +13,12 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        
         String[] commands = new String[]{"Notepad.exe"};
-        Process process;
-        int exitCode;
         try{
-            process = Runtime.getRuntime().exec(commands);
+            Runtime.getRuntime().exec(commands);
             System.out.println("Se ha lanzado el proceso correctamente");
-            exitCode = process.waitFor();
-            System.out.println("El proceso ha finalizado con el codigo " + exitCode);
         }
         catch(Exception ex){
             ex.printStackTrace();
